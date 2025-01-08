d0bba1340e0ea9ec9c7901c2f028190364b1db2f
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-08 15:55:54 -0800

    Added example of unit tests with coverage.

---
+++ 01.08-correctness/fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    """
+    Compute Fibonacci numbers.
+
+    :param n: A non-negative integer
+    :return: The n'th Fibonacci number
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
--- 01.08-correctness/fibonacci_tests.py
+++ 01.08-correctness/fibonacci_tests.py
@@ -6,6 +6,12 @@ class TestFibonacci(unittest.TestCase):
     def test01(self):
         self.assertEqual(fib.fibonacci(0), 0)
 
+    def test02(self):
+        self.assertEqual(fib.fibonacci(1), 1)
+
+    def test03(self):
+        self.assertEqual(fib.fibonacci(4), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
