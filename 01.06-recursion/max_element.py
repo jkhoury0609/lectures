7161062ab835682602121dfbfccf8c89d3ff1f30
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-08 11:55:30 -0800

    Added example of unit tests with coverage.

--- 01.06-recursion/max_element.py
+++ 01.06-recursion/max_element.py
@@ -1,19 +1,19 @@
 def max_element(lst):
     """
-    Recursion is the definition of a value in terms of itself, and recursive
-     functions are those that call themselves. A function ought to take as
-     arguments an instance of a problem, and produce as return value a
-     solution to that problem. Thus, a recursive function must identify a
-     smaller version of the *same* problem.
+    Recursion is the definition of a value in terms of itself, and a recursive
+     functions is one that calls itself. A function ought to take as arguments
+     an instance of a problem, and produce as return value a solution to that
+     problem. Thus, a recursive function must identify a smaller version of the
+     *same* problem.
 
     :param lst: A list of comparable elements
     :return: The largest element, or None if empty.
     """
     print("lst: %r" % lst)
 
-    # Here, a shorter list is a smaller version of the same problem -- we still
-    #  have the same type of argument and we still expect the same type of
-    #  return value, we just have one fewer element in list:
+    # Here, the smallest possible problems are the lists of zero and one
+    #  elements. They have two different solutions, so they require two
+    #  different cases:
     if len(lst) == 0:
         return None
     elif len(lst) == 1:
