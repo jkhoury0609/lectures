219e349330c2dddaa633b52474654d228e1c8890
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-15 12:00:33 -0800

    Added pseudocode for linked lists.

--- 01.15-linked-lists/pair.py
+++ 01.15-linked-lists/pair.py
@@ -1,3 +1,9 @@
+# NOTE: A pair is very similar to a list, in that pairs are also finite,
+#       ordered collections of elements. The only difference is that pairs can
+#       only contain precisely two elements. We could define a "triple" that
+#       contains precisely three elements, but then we would just have the same
+#       problem for a collection of four elements...
+
 class Pair:
     """ A pair of elements """
 
@@ -12,3 +18,9 @@ class Pair:
 
     def __repr__(self):
         return "Pair(%r, %r)" % (self.first, self.second)
+
+
+# NOTE: ...alternatively, since the elements of a pair may have any type, the
+#       second element of a pair could be *another pair*. We can then create
+#       arbitrarily large collections by nesting arbitrarily many pairs:
+lst = Pair('a', Pair('b', Pair('c', None)))
