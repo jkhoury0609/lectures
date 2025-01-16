1e94f9d87980783940170b2fbf941a8c487ddd17
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-15 16:01:19 -0800

    Added pseudocode for linked lists.

--- 01.15-linked-lists/linked_list.py
+++ 01.15-linked-lists/linked_list.py
@@ -19,14 +19,45 @@ class Node:
 
 
 def get(lst, idx):
+    # NOTE: This is the general pattern any time we wish to "traverse" a linked
+    #       list. In order to access any node within the list, we must start
+    #       at the head and repeatedly access the next node.
+    #
+    # Start with a current node being the given lst's head.
+    # For i from 0 to the given idx:
+    #     Set the current node to the current node's next.
+    # Return the current node's value.
     pass
 
 
 def set(lst, idx, value):
+    # Start with a current node being the given lst's head.
+    # For i from 0 to the given idx:
+    #     Set the current node to the current node's next.
+    # Set the current node's value to the given value.
     pass
 
 
 def add(lst, idx, value):
+    # Create a new node containing the given value.
+    # If the given idx is 0, then:
+    #    NOTE: Adding to the beginning of a linked list is a special case,
+    #          because in this scenario there is no previous node to be found;
+    #          rather, we need to modify the head of the list.
+    #
+    #    Set the new node's next to the given lst's head.
+    #    Set the given lst's head to the new node.
+    # Else, do:
+    #    NOTE: In general, adding a node to a linked list requires first
+    #          finding the previous node in the list. The previous node will
+    #          need a link to the new node once it is added.
+    #
+    #    Start with a current node being the given lst's head.
+    #    For i from 0 to the given idx - 1:
+    #        Set the current node to the current node's next.
+    #    Set the new node's next to current node's next.
+    #    Set the current node's next to the new node.
+    # Increment the given lst's size.
     pass
 
 
--- 01.15-linked-lists/pair.py
+++ 01.15-linked-lists/pair.py
@@ -1,3 +1,9 @@
+# NOTE: Just like a list, a pair is a finite, ordered collection of elements.
+#       However, lists must be able to contain arbitrarily many elements, and
+#       pairs can only contain exactly two. If we wanted to contain three
+#       elements, we could add a third attribute, but then we would just have
+#       exactly the same problem with four elements...
+
 class Pair:
     """ A pair of elements """
 
@@ -12,3 +18,8 @@ class Pair:
 
     def __repr__(self):
         return "Pair(%r, %r)" % (self.first, self.second)
+
+# NOTE: ...instead, since the elements of a pair can have any type, the second
+#       element could be *another pair*. We can then contain arbitrarily many
+#       elements by nesting arbitrarily many pairs:
+lst = Pair('a', Pair('b', Pair('c', None)))
