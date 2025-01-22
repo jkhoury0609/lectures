0f527e050d3169a760be5ef6048439c1c65b5e5a
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-22 15:58:14 -0800

    Added pseudocode for stacks.

--- 01.22-stacks/linked_stack.py
+++ 01.22-stacks/linked_stack.py
@@ -19,12 +19,29 @@ class Node:
 
 
 def push(stack, value):
+    # NOTE: A stack is a list that can only be operated on at one terminus.
+    #       Adding and removing at the beginning of a linked list is O(1), but
+    #       adding and removing at the end of a linked list is O(n). Thus, the
+    #       beginning of the linked list ought to be the top-of-stack.
+    #
+    # Create a new node containing the given value.
+    # Set the new node's next to the given stack's head.
+    # Set the given stack's head to the new node.
+    # Increment the given stack's size.
     pass
 
 
 def pop(stack):
+    # NOTE: Essentially, operating on a linked stack is the same as operating
+    #       on a linked list if the given index was always known to be 0.
+    #
+    # Set a temp variable to the given stack's head's value.
+    # Set the given stack's head to the given stack's head's next.
+    # Decrement the given stack's size.
+    # Return the temp variable.
     pass
 
 
 def peek(stack):
+    # Return the given stack's head's value.
     pass
