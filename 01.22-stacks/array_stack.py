e762c3242928686d1da1bf1ba9fb445dddb4a76b
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-22 10:05:43 -0800

    Added base code for stacks.

---
+++ 01.22-stacks/array_stack.py
@@ -0,0 +1,22 @@
+class Stack:
+    """ A LIFO collection of elements """
+
+    def __init__(self):
+        # The length of the backing array:
+        self.capacity = 4
+        # The backing array:
+        self.array = [None] * self.capacity
+        # The number of elements in this stack:
+        self.size = 0
+
+
+def push(stack, value):
+    pass
+
+
+def pop(stack):
+    pass
+
+
+def peek(stack):
+    pass
---
+++ 01.22-stacks/linked_stack.py
@@ -0,0 +1,30 @@
+class Stack:
+    """ A LIFO collection of elements """
+
+    def __init__(self):
+        # The head of the backing linked list:
+        self.head = None
+        # The number of elements in this stack:
+        self.size = 0
+
+
+class Node:
+    """ A single node in a linked list """
+
+    def __init__(self, value, next):
+        # The value contained in this node:
+        self.value = value
+        # The next node in the linked list:
+        self.next = next
+
+
+def push(stack, value):
+    pass
+
+
+def pop(stack):
+    pass
+
+
+def peek(stack):
+    pass
