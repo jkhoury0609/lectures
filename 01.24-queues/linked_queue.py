4b3a58a026da79663c231b4c4901d0b1fac2af0f
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-24 15:55:17 -0800

    Added pseudocode for linked queues.

--- 01.24-queues/linked_queue.py
+++ 01.24-queues/linked_queue.py
@@ -21,12 +21,39 @@ class Node:
 
 
 def enqueue(queue, value):
+    # NOTE: Given both references to the head and tail nodes, adding to the
+    #       beginning and the end of a linked list are both O(1). Arguably, for
+    #       the purposes of enqueueing, it does not matter which terminus of
+    #       the list is the back of the queue...
+    #
+    # Create a new node containing the given value.
+    #
+    # If the given queue's size is 0, then:
+    #     Set the given queue's head to the new node.
+    # Else, do:
+    #     Set the given queue's tail's next to the new node.
+    #
+    # Set the given queue's tail to the new node.
+    # Set the new node's next to None.
+    # Increment the given queue's size.
     pass
 
 
-def dequeue(queue, value):
+def dequeue(queue):
+    # NOTE: ...but even given both references to the head and tail nodes,
+    #       removing from the beginning of a linked list is O(1) while from the
+    #       end is O(n). We would like the head of the list to be the front of
+    #       the queue, thus, the tail must be the back.
+    #
+    # If the given queue's size is 1, then:
+    #     Set the given queue's head and tail to None.
+    # Else, do:
+    #     Set the given queue's head to the given queue's head's next.
+    #
+    # Decrement the given queue's size.
     pass
 
 
 def peek(queue):
+    # Return the given queue's head's value.
     pass
