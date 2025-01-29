3168683834f3e4bde2aeb444d2f4ddac0636ab01
Christopher E. Siu <cesiu@calpoly.edu>
2025-01-29 10:03:44 -0800

    Added base code for searching.

---
+++ 01.29-searching/sorted_list.py
@@ -0,0 +1,26 @@
+class SortedList:
+    """ A sorted collection of elements """
+
+    def __init__(self):
+        # The length of the backing array:
+        self.capacity = 4
+        # The backing array:
+        self.array = [None] * self.capacity
+        # The number of elements in this sorted list:
+        self.size = 0
+
+
+def insert(lst, value):
+    pass
+
+
+def remove(lst, idx):
+    pass
+
+
+def find(lst, value):
+    pass
+
+
+def create(array, size):
+    pass
